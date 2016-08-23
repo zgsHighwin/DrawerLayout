c68c797c8c3bd60a89472716b4bdee05173a69a7
zgsHighwin <qdw1593276@163.com>
2016-08-23 13:52:34 +0800

    “”

--- app/src/main/java/highwin/zgs/drawerlayout/ToastUtils.java
+++ app/src/main/java/highwin/zgs/drawerlayout/ToastUtils.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 /**
  * User: zgsHighwin
  * Email: 799174081@qq.com Or 799174081@gmail.com
- * Description: 让toast不会待一个一个弹出｜to make toast not to wait
+ * Description: 让toast不会待一个一个弹出,单例模式的toast｜to make toast not to wait
  * Create-Time: 2016/8/23 10:50
  */
 public class ToastUtils {
